c8fec9fa0d268e3990e5be0d0ae85ab148b23911
Renate <re.ubartas@googlemail.com>
2021-11-30 15:05:22 +0100

    testing branch

--- PuF_Wizard/src/application/Main.java
+++ PuF_Wizard/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-						
+			// HALLO 			
 			Pane root = FXMLLoader.load(getClass().getResource("/view/mockup_pane.fxml"));
 			
 			Scene scene = new Scene(root);
